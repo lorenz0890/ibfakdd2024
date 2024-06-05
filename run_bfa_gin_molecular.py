6a876b8b7145ee8293e7d349cd17779c07d2943f
lorenz <lorenz.kummer@univie.ac.at>
2024-06-05 16:54:36 +0200

    update

---
+++ run_bfa_gin_molecular.py
@@ -0,0 +1,257 @@
+import traceback
+
+import torch.nn
+from torch_geometric.nn.conv import WLConv
+
+from app.graph.gin_molhiv_moltox_quantization import *
+from ogb.graphproppred import PygGraphPropPredDataset as ogb_datasets
+from torch_geometric.data import DataLoader as GMDataLoader
+from pyq.fia.bfa.attack_gin_molhiv_molpcba.RBFA import RandomBFA as RBFA
+from pyq.fia.bfa.attack_gin_molhiv_molpcba.PBFA import ProgressiveBFA as PBFA
+from pyq.fia.bfa.attack_gin_molhiv_molpcba.IBFA import InjectivityBFA as IBFA
+import datetime
+import time
+import random
+from ogb.graphproppred import Evaluator
+
+from pyq.fia.bfa.utils import _WrappedGraphDataset, eval_ogbg, get_n_params
+
+import argparse #new
+import copy
+# import json
+
+#from pyq.pyq.core.quantization.wrapper import TransformationLayerQuantizerWrapper
+
+
+def setup_data(dataset_name, batch_size, shuffle=True):
+    dataset = ogb_datasets(name=dataset_name)
+    split_idx = dataset.get_idx_split()
+    train_data = dataset[split_idx["train"]]
+    #valid_data = dataset[split_idx["valid"]]
+    test_data = dataset[split_idx["test"]]
+    train_loader = GMDataLoader(dataset=_WrappedGraphDataset(train_data, None),
+                                batch_size=batch_size,
+                                shuffle=shuffle,
+                                num_workers=12, pin_memory=True)
+    test_loader = GMDataLoader(dataset=_WrappedGraphDataset(test_data, None),
+                               batch_size=batch_size,
+                               shuffle=shuffle,
+                               num_workers=12, pin_memory=True)
+    return train_loader, test_loader
+
+def setup_net(dataset_name):
+    if dataset_name == 'ogbg-molpcba':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molpcba.pth")['Model']
+    elif dataset_name == 'ogbg-molhiv':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molhiv.pth")['Model']
+    elif dataset_name == 'ogbg-moltox21':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_moltox.pth")['Model']
+    elif dataset_name == 'ogbg-molclintox':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molclintox.pth")['Model']
+    elif dataset_name == 'ogbg-moltoxcast':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_moltoxcast.pth")['Model']
+    elif dataset_name == 'ogbg-molbace':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molbace.pth")['Model']
+    elif dataset_name == 'ogbg-molbbbp':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molbbbp.pth")['Model']
+    elif dataset_name == 'ogbg-molsider':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molsider.pth")['Model']
+    elif dataset_name == 'ogbg-molmuv':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_molmuv.pth")['Model']
+    net.eval()
+    return net
+
+def setup_attack(attack_type, dataset):
+    BFA, criterion, data, target = None, None, None, None
+    if attack_type == 'RBFA':
+        BFA = RBFA
+        criterion = torch.nn.BCEWithLogitsLoss()
+    if attack_type == 'PBFA':
+        BFA = PBFA
+        criterion = torch.nn.BCEWithLogitsLoss()
+    if attack_type in ['IBFAv1', 'IBFAv2']:
+        BFA = IBFA
+        if dataset in ['ogbg-molpcba']:
+            criterion = torch.nn.KLDivLoss()
+        if dataset in ['ogbg-moltox21', 'ogbg-moltoxcast', 'ogbg-molsider', 'ogbg-molclintox', 'ogbg-molmuv']:
+            criterion = torch.nn.KLDivLoss(log_target=True)
+        #L1 doesnt work well for molpcba
+        if dataset in ['ogbg-molhiv', 'ogbg-molbace', 'ogbg-molbbbp']:
+            criterion = torch.nn.L1Loss()# L1 works better for molhiv than KLDDiv
+    return BFA, criterion
+
+import torch.nn.functional as F
+
+def main():
+    parser = argparse.ArgumentParser(prog='python run_bfa_gin_molecular.py')
+    parser.add_argument('--type', help='PBFA, RBFA, IBFAv1, IBFAv2', type=str)
+    parser.add_argument('--data', help='ogbg-molpcba, ogbg-molhiv, ogbg-moltox21, ogbg-molclintox, ogbg-moltoxcast, ogbg-molbace', type=str)
+    parser.add_argument('--n', help='Run experiment n times', type=int)
+    parser.add_argument('--k', help='Run BFA k times', type=int)
+    parser.add_argument('--sz', help='Batch size', type=int)
+    #parser.print_help()
+    args = parser.parse_args()
+    print(args.type, args.data, args.n, args.k, args.sz)
+
+    attack_type, dataset_name, device = args.type, args.data, torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    experiment_runs, eval_runs, attack_runs, batch_size = args.n, 10, args.k, args.sz
+
+    #attack_type, dataset_name, device = 'RBFA', 'ogbg-molpcba', torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #experiment_runs, eval_runs, attack_runs, batch_size = 10, 10, 10, 256
+    pre_metric, post_metric, bit_flips = [], [], []
+
+    print('Start time', datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    experiment_accumulated_seconds = 0
+    failure_counter = 0
+    train_loader, test_loader = setup_data(dataset_name, batch_size, True) #shuffled every epoch -> should be False
+    evaluator = Evaluator(dataset_name)
+
+    for r in range(experiment_runs):
+        start_time = time.time()
+        net = setup_net(dataset_name).to(device)#copy.deepcopy(net_clean).to(device)
+        #net.eval()
+        BFA, criterion = setup_attack(attack_type, dataset_name)
+        print('params', get_n_params(net))
+        data, target = None, None
+        if attack_type == 'PBFA':
+            with torch.no_grad():
+                x = random.randint(0, int(len(train_loader.dataset)/batch_size))
+                for i, data in enumerate(train_loader):
+                    if i == x:
+                        print('PBFA data found', flush=True)
+                        data = data.to(device)
+                        target = data.y
+                        break
+        if attack_type == 'RBFA':
+            pass
+        if attack_type == 'IBFAv1':
+            with torch.no_grad():
+                max_loss = 0
+                d = train_loader#d = [x for x in train_loader]
+                for i, data1 in enumerate(d):
+                    for j, data2 in enumerate(d):
+                        #print(data1.size(), data2.size(), flush=True)
+                        if True: #data1.size() == data2.size() and i != j:
+                            data1 = data1.to(device)
+                            data2 = data2.to(device)
+
+                            is_labeled1, is_labeled2 = data1.y == data1.y, data2.y == data2.y # Not all data always labeled.
+                            out1, out2 = net(data1)[is_labeled1], net(data2)[is_labeled2]
+                            cut = min(out1.shape[0], out2.shape[0])
+                            #print(out1, out2)
+                            loss = criterion(out1[:cut], out2[:cut])
+                            #print(loss, flush=True)
+                            if max_loss < loss:
+                                #print(i, loss)
+                                max_loss = loss
+                                max_data1 = data1
+                                max_data2 = data2
+                    if i > 10:
+                        break
+            print(max_data1.size(), max_data2.size(), flush=True)
+            print('IBFAv1 data found', flush=True)
+            data = max_data1
+            target = max_data2
+
+        net = net.to(device)
+
+        test_perf = eval_ogbg(net, device, test_loader, evaluator)
+        #statsdict_pre = copy.deepcopy(statsdict)
+        #for handle in handles: handle.remove()
+        #print(statsdict)
+        pre_metric.append(list(test_perf.values())[0])
+        attacker, attack_log = BFA(criterion, net, 100, True), None
+
+        flips = 0
+        logs = []
+        try:
+            if attack_type in ['RBFA', 'PBFA', 'IBFAv1']:
+                for i in range(attack_runs):
+                    attack_log = attacker.run_attack(net, data, target, max_flips=args.k)
+                    print(attack_log)
+                    if len(attack_log) > 0:
+                        flips = attack_log[-1][1]
+                    else:
+                        break
+            if attack_type == 'IBFAv2':
+                for _ in range(attack_runs):
+                    with torch.no_grad():
+                        max_loss = 0
+                        for i, data1 in enumerate(train_loader):
+                            for j, data2 in enumerate(train_loader):
+                                if True: #data1.size() == data2.size() and i != j:
+                                    data1 = data1.to(device)
+                                    data2 = data2.to(device)
+
+                                    is_labeled1, is_labeled2 = data1.y == data1.y, data2.y == data2.y  # Not all data always labeled.
+                                    out1, out2 = net(data1)[is_labeled1], net(data2)[is_labeled2]
+                                    cut = min(out1.shape[0], out2.shape[0])
+                                    loss = criterion(out1[:cut], out2[:cut])
+
+                                    if max_loss < loss:
+                                        max_loss = loss
+                                        max_data1 = data1
+                                        max_data2 = data2
+
+                            if i > 10:
+                                break
+                    attack_log = attacker.run_attack(net, max_data1.to(device), max_data2.to(device), max_flips=args.k)
+                    #name = attack_log[0][2]
+                    if len(attack_log) > 0:
+                        flips = attack_log[-1][1]
+                    else:
+                        break
+            if attack_log is None:
+                raise Exception('No attack solution found')
+            # bit_flips.append(attack_log[-1][1])
+            bit_flips.append(flips)
+            #train_perf = eval_ogbg(net, device, train_loader, evaluator)
+            d = [0]
+            #statsdict = {}
+            #hook_all_layers(net, d, collect_stats, True)
+            test_perf = eval_ogbg(net, device, test_loader, evaluator)
+            #intra_sim, inter_sim = [],[]
+            #for key in statsdict:
+            #    if 'inter' in key:
+            #        inter_sim.append(np.array(np.array(statsdict[key]).mean()/statsdict_pre[key]).mean())
+            #    if 'intra' in key:
+            #        intra_sim.append(np.array(statsdict[key]).mean()/np.array(statsdict_pre[key]).mean())
+            #    #print(np.array(statsdict[key]).mean(), np.array(statsdict_pre[key]).mean())
+            #    #print(key, np.array(statsdict_pre[key]).mean(), np.array(statsdict[key]).mean())
+            #print('inter', np.array(inter_sim).mean(), 'inter', np.array(intra_sim).mean())
+            #for handle in handles: handle.remove()
+            #print(test_perf.values())
+            post_metric.append(list(test_perf.values())[0])
+        except Exception as e:
+            failure_counter+=1
+            print(failure_counter, e.args)
+            print(traceback.format_exc())
+        ct = datetime.datetime.now()
+        experiment_accumulated_seconds += time.time() - start_time
+
+        #print(layers)
+        print('Current time:', ct.strftime("%d/%m/%Y, %H:%M:%S"),
+              'Completed:', (r + 1) / experiment_runs * 100, '%',
+              'Duration per experiment:', round(time.time() - start_time, 2), 's',
+              'ETA:', (ct+datetime.timedelta(seconds=((experiment_accumulated_seconds/(r+1)) * (experiment_runs-r-1)))).strftime("%d/%m/%Y, %H:%M:%S")
+        )
+    print('Pre and post attack average metric, bitflips, failures',
+          sum(pre_metric) / len(pre_metric),
+          sum(post_metric) / len(post_metric),
+          sum(bit_flips) / len(bit_flips),
+          failure_counter
+          )
+    print('End time', datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    exit(-1)
+    '''
+    json_string = json.dumps({'pre' : sum(pre_metric) / len(pre_metric),
+     'post' : sum(post_metric) / len(post_metric),
+     'flips' : sum(bit_flips) / len(bit_flips),
+     'fails' : failure_counter
+     })
+
+    with open("{}_{}_{}_{}_{}_{}.json".format(attack_type, dataset_name, device, experiment_runs, attack_runs, batch_size), 'w') as outfile:
+        outfile.write(json_string)
+    '''
+if __name__ == "__main__":
+    main()
---
+++ run_bfa_gin_planetoid.py
@@ -0,0 +1,230 @@
+import random
+
+import torch.nn
+from torch_geometric.datasets import WebKB
+
+from app.graph.gin_cora_quantization import *
+from torch_geometric.data import DataLoader as GMDataLoader
+from pyq.fia.bfa.attack_gin_cora.RBFA import RandomBFA as RBFA
+from pyq.fia.bfa.attack_gin_cora.PBFA import ProgressiveBFA as PBFA
+from pyq.fia.bfa.attack_gin_cora.IBFA import InjectivityBFA as IBFA
+import datetime
+import time
+import traceback
+
+from pyq.fia.bfa.utils import _WrappedGraphDataset, evaluate_cora
+
+def setup_net(dataset_name):
+    if dataset_name == 'Cora':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_cora.pth")['Model']
+    elif dataset_name == 'PubMed':
+        net = torch.load("/media/lorenz/Volume/code/pyq_main/pyq/app/graph/pyq_model_gin_pubmed.pth")['Model']
+    net.eval()
+    return net
+
+def setup_attack(attack_type):
+    BFA, criterion, data, target = None, None, None, None
+    if attack_type == 'RBFA':
+        BFA = RBFA
+        criterion = F.cross_entropy#torch.nn.BCEWithLogitsLoss()
+    if attack_type == 'PBFA':
+        BFA = PBFA
+        criterion = F.cross_entropy#torch.nn.BCEWithLogitsLoss()
+    if attack_type in ['IBFAv1', 'IBFAv2']:
+        BFA = IBFA
+        criterion = torch.nn.KLDivLoss(log_target=True)
+    return BFA, criterion
+
+@torch.no_grad()
+def validate(model, data, fold=0):
+    model.eval()
+    pred = model(data.x, data.edge_index, data.edge_weight).argmax(dim=-1)
+
+    accs = []
+    for mask in [data.train_mask, data.val_mask, data.test_mask]:
+        accs.append(int((pred[mask] == data.y[mask]).sum()) / int(mask.sum()))
+    return accs
+
+def main():
+    parser = argparse.ArgumentParser(prog='python run_bfa_gin_planetoid.py')
+    parser.add_argument('--type', help='PBFA, RBFA, IBFAv1, IBFAv2', type=str)
+    parser.add_argument('--data', help='Wisconsin, Texas', type=str)
+    parser.add_argument('--n', help='Run experiment n times', type=int)
+    parser.add_argument('--k', help='Run BFA k times', type=int)
+    parser.add_argument('--sz', help='Batch size', type=int)
+    #parser.add_argument('--f', help='Fold for which to evaluate', type=int)
+    fold = None # Not used for Cora/PubMed, kept here as dummy.
+    args = parser.parse_args()
+    print(args.type, args.data, args.n, args.k, args.sz)
+
+    # attack_type, device = 'PBFA', torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # experiment_runs, eval_runs, attack_runs, batch_size = 1, 10, 100, 32
+    attack_type, dataset_name, device = args.type, args.data, torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    experiment_runs, eval_runs, attack_runs, batch_size = args.n, 10, args.k, args.sz
+    pre_acc, post_acc, bit_flips = [], [], []
+
+    print('Start time', datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    experiment_accumulated_seconds = 0
+    failure_counter = 0
+
+    for r in range(experiment_runs):
+        start_time = time.time()
+
+        net = setup_net(dataset_name).to(device)#copy.deepcopy(net_clean).to(device)
+        BFA, criterion = setup_attack(attack_type)
+
+        dataset = dataset_name#"PubMed" # 'Cora
+        path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'Planetoid')
+        dataset = Planetoid(path, dataset, transform=T.NormalizeFeatures())[0]
+
+        dataset = dataset.to(device)
+        #dataset.x = dataset.x[:, :10]
+        print(dataset.train_mask)
+        if attack_type == 'PBFA':
+            with torch.no_grad():
+
+                true_indices = torch.where(dataset.train_mask)[0]
+                if batch_size < len(true_indices):
+                    selected_indices = true_indices[torch.randperm(len(true_indices))[:batch_size]]
+                else:
+                    selected_indices = true_indices
+                dataset.train_mask = torch.zeros_like(dataset.train_mask, dtype=torch.bool)
+                dataset.train_mask[selected_indices] = True
+
+                #max_multiple = (dataset.train_mask.shape[0] - batch_size) // batch_size
+                #random_int = random.randint(0, max_multiple)
+                #target1 = random_int * batch_size
+                #target2 = None
+                #print(dataset.train_mask, max_multiple)
+        target1 = None #random_int * batch_size
+        target2 = None
+        print(dataset.train_mask)
+        #exit()
+
+        if attack_type == 'RBFA':
+            target1, target2 = None, None
+        if attack_type == 'IBFAv1':
+            train_mask_orig = dataset.train_mask.clone()
+            out1_orig = net(dataset.x, dataset.edge_index, dataset.edge_weight)
+            out2_orig = net(dataset.x, dataset.edge_index, dataset.edge_weight)
+            with torch.no_grad():
+                max_loss = float('-inf')
+                for k in range(0, dataset.train_mask.shape[0]**2, batch_size):
+                    true_indices = torch.where(train_mask_orig)[0]
+                    if batch_size < len(true_indices):
+                        selected_indices = true_indices[torch.randperm(len(true_indices))[:batch_size]]
+                    else:
+                        selected_indices = true_indices
+                    mask1 = torch.zeros_like(train_mask_orig, dtype=torch.bool)
+                    mask1[selected_indices] = True
+
+                    if batch_size < len(true_indices):
+                        selected_indices = true_indices[torch.randperm(len(true_indices))[:batch_size]]
+                    else:
+                        selected_indices = true_indices
+                    mask2 = torch.zeros_like(train_mask_orig, dtype=torch.bool)
+                    mask2[selected_indices] = True
+
+                    out1 = out1_orig[mask1]#[k:k + batch_size, :]
+                    out2 = out2_orig[mask2]
+                    if out1.shape[0] != out2.shape[0]:
+                        continue
+                    loss = criterion(out1, out2)
+                    #print(loss)
+
+                    if max_loss < loss and not torch.isnan(loss):
+                        print(round(k/dataset.train_mask.shape[0]**2*100, 4), loss)
+                        max_loss = loss
+                        target1 = mask1
+                        target2 = mask2
+                    if k > batch_size*25000:
+                        break
+
+        net = net.to(device)
+
+        print(validate(net, dataset, fold))
+        #exit()
+        pre_acc.append(validate(net, dataset, fold)[2])
+        attacker, attack_log = BFA(criterion, net, 5, True), None #10 Texas, 5 Wisconsin - prevents PBFA from being stuck, 5 Cora/Pubmed
+
+        flips = 0
+        try:
+            if attack_type in ['RBFA', 'PBFA', 'IBFAv1']:
+                for i in range(attack_runs):
+                    attack_log = attacker.run_attack(net, dataset, target1, target2, batch_size, attack_runs, fold)
+                    if len(attack_log) > 0:
+                        flips = attack_log[-1][1]
+                    else:
+                        break
+
+            if attack_type == 'IBFAv2':
+                train_mask_orig = dataset.train_mask.clone()
+                for i in range(attack_runs):
+                    out1_orig = net(dataset.x, dataset.edge_index, dataset.edge_weight)
+                    out2_orig = net(dataset.x, dataset.edge_index, dataset.edge_weight)
+                    with torch.no_grad():
+                        max_loss = float('-inf')
+                        for k in range(0, dataset.train_mask.shape[0]**2, batch_size):
+                            true_indices = torch.where(train_mask_orig)[0]
+                            if batch_size < len(true_indices):
+                                selected_indices = true_indices[torch.randperm(len(true_indices))[:batch_size]]
+                            else:
+                                selected_indices = true_indices
+                            mask1 = torch.zeros_like(train_mask_orig, dtype=torch.bool)
+                            mask1[selected_indices] = True
+
+                            if batch_size < len(true_indices):
+                                selected_indices = true_indices[torch.randperm(len(true_indices))[:batch_size]]
+                            else:
+                                selected_indices = true_indices
+                            mask2 = torch.zeros_like(train_mask_orig, dtype=torch.bool)
+                            mask2[selected_indices] = True
+
+                            out1 = out1_orig[mask1]  # [k:k + batch_size, :]
+                            out2 = out2_orig[mask2]
+                            if out1.shape[0] != out2.shape[0]:
+                                continue
+                            loss = criterion(out1, out2)
+                            #print(loss)
+
+                            if max_loss < loss and not torch.isnan(loss):
+                                print(k, loss)
+                                max_loss = loss
+                                target1 = mask1
+                                target2 = mask2
+                            if k > batch_size*10000:
+                                break
+
+                    attack_log = attacker.run_attack(net, dataset, target1, target2, batch_size, attack_runs, fold)
+                    if len(attack_log) > 0:
+                        flips = attack_log[-1][1]
+                    else:
+                        break
+            if attack_log is None:
+                raise Exception('No attack solution found')
+
+            bit_flips.append(flips)
+            post_acc.append(validate(net, dataset, fold)[2])
+        except Exception as e:
+            failure_counter+=1
+            print(failure_counter, e.args)
+            print(traceback.format_exc())
+            exit(-1)
+        ct = datetime.datetime.now()
+        experiment_accumulated_seconds += time.time() - start_time
+        #print(pre_acc)
+        #print(post_acc)
+        print('Current time:', ct.strftime("%d/%m/%Y, %H:%M:%S"),
+              'Completed:', (r + 1) / experiment_runs * 100, '%',
+              'Duration per experiment:', round(time.time() - start_time, 2), 's',
+              'ETA:', (ct+datetime.timedelta(seconds=((experiment_accumulated_seconds/(r+1)) * (experiment_runs-r-1)))).strftime("%d/%m/%Y, %H:%M:%S")
+        )
+    print('Pre and post attack average ACC, bitflips, failures',
+          sum(pre_acc) / len(pre_acc),
+          sum(post_acc) / len(post_acc),
+          sum(bit_flips) / len(bit_flips),
+          failure_counter
+          )
+    print('Start time', datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+if __name__ == "__main__":
+    main()
